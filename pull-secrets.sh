75de8be6538915171d205a4da5b86d2a881be946
Hidetatsu Yaginuma <ygnmhdtt@gmail.com>
2019-05-20 10:38:27 +0900

    update

--- pull-secrets.sh
+++ pull-secrets.sh
@@ -7,14 +7,15 @@ export PATH=$PATH:$HOME/.config/google-cloud-sdk/bin
 echo "Download secrets from GCS, need authentication"
 gcloud auth login
 
-dest="$HOME/ghq/src/github.com/yagi5/dotfiles/.config/secrets"
+base="$HOME/ghq/src/github.com/yagi5/dotfiles/.config"
+dest="$base/secrets"
 
 [ -e $HOME/.config/ssh/config ] && rm $HOME/.config/ssh/config
 [ -e $HOME/.config/ssh/github_mac ] && rm $HOME/.config/ssh/github_mac
 [ -e $HOME/.config/ssh/known_hosts ] && rm $HOME/.config/ssh/known_hosts
 [ -e $HOME/.config/bash/profile.pvt ] && rm $HOME/.config/bash/profile.pvt
 [ -e $dest/ghq.private ] && rm $dest/ghq.private
-[ -e $dest/hist-datastore.json ] && rm $dest/hist-datastore.json
+[ -e $dest/hist-datastore.json ] && rm $base/hist-datastore.json
 
 echo "Pulling secrets"
 gsutil cp gs://blackhole-yagi5/config $dest
@@ -22,7 +23,7 @@ gsutil cp gs://blackhole-yagi5/known_hosts $dest
 gsutil cp gs://blackhole-yagi5/github_mac $dest
 gsutil cp gs://blackhole-yagi5/profile.pvt $dest
 gsutil cp gs://blackhole-yagi5/ghq.private $dest
-gsutil cp gs://blackhole-yagi5/hist-datastore.json $dest
+gsutil cp gs://blackhole-yagi5/hist-datastore.json $base
 
 chmod 600 $dest/github_mac
 
